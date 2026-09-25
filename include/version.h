af805a65b9cc1fdb7550df761a79c2c1c78bb317
Marcel Verpaalen <marcel@verpaalen.com>
2026-09-25 19:58:49 +0200

    Set firmware version 0.1.0 for the first stable release

--- include/version.h
+++ include/version.h
@@ -2,7 +2,7 @@
 
 #pragma once
 
-constexpr const char *FIRMWARE_VERSION = "0.1.0-dev";
+constexpr const char *FIRMWARE_VERSION = "0.1.0";
 
 // __DATE__ / __TIME__ are compiler-substituted at the point of translation
 // (main.cpp / rest_routes.cpp), so the printable build timestamp is built
